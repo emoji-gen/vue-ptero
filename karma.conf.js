db8c93cf2d8eb5dbaa3a9fb2f70f45aff5bae6dd
Pine Mizune <pinemz@gmail.com>
2017-01-31 00:51:36 +0900

    test(karma): add tests

---
+++ karma.conf.js
@@ -0,0 +1,93 @@
+'use strict'
+
+const path = require('path')
+
+const customLaunchers = {
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 10',
+    version: '51.0'
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    platform: 'Windows 10',
+    version: '47.0'
+  },
+  sl_ie: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '11.0',
+  },
+  sl_edge: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '13.10586'
+  },
+  sl_safari: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '9.0',
+  },
+}
+
+const isTravis   = process.env.TRAVIS === 'true'
+const isFirstJob = /\.1$/.test(process.env.TRAVIS_JOB_NUMBER)
+
+const browsers = ['PhantomJS']
+if (isTravis && isFirstJob) {
+  Array.prototype.push.apply(browsers, Object.keys(customLaunchers))
+}
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'sinon-chai'],
+
+    browsers: browsers,
+    customLaunchers: customLaunchers,
+
+    sauceLabs: {
+      public: 'public',
+    },
+    concurrency: isTravis ? 5 : Infinity,
+    browserDisconnectTimeout: 10000,
+    browserNoActivityTimeout: 10000,
+    browserDisconnectTolerance: 16,
+
+    files: [
+      'test.js'
+    ],
+    exclude: [],
+
+    preprocessors: {
+      'test.js': ['webpack']
+    },
+
+    webpack: {
+      resolve: {
+        extensions: ['', '.js', '.json']
+      },
+      module: {
+      },
+      node: {
+        fs: 'empty',
+      },
+    },
+    webpackMiddleware: {
+      noInfo: true
+    },
+
+    reporters: ['mocha'],
+
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_LOG,
+
+    autoWatch: true,
+  })
+}
---
+++ test.js
@@ -0,0 +1,43 @@
+var expect   = require('chai').expect
+var VuePtero = require('./index')
+
+describe('index', function () {
+  var Vue
+
+  beforeEach(function () {
+    Vue = function () { }
+    Vue.use = function (plugin, options) {
+      plugin.install(Vue, options)
+    }
+  })
+
+  describe('install', function () {
+    describe('without options', function () {
+      it('should be able to emit and listen custom events', function () {
+        Vue.use(VuePtero)
+
+        var vm = new Vue()
+        expect(vm.$ptero).to.be.ok
+
+        var spy = sinon.spy()
+        vm.$ptero.on('event-xxx-1', spy)
+        vm.$ptero.emit('event-xxx-1')
+        expect(spy).to.have.been.calledOnce
+      })
+    })
+
+    describe('with options', function () {
+      it('should be able to emit and listen custom events', function () {
+        Vue.use(VuePtero, { target: document.body })
+
+        var vm = new Vue()
+        expect(vm.$ptero).to.be.ok
+
+        var spy = sinon.spy()
+        vm.$ptero.on('event-xxx-2', spy)
+        vm.$ptero.emit('event-xxx-2')
+        expect(spy).to.have.been.calledOnce
+      })
+    })
+  })
+})
