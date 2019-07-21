83e7acb34bcb45e63e83858ab5203c8b587c0a98
Pine Mizune <pinemz@gmail.com>
2019-07-21 16:20:50 +0900

    fix: update karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -2,54 +2,13 @@
 
 const path = require('path')
 
-const sauceLabsLaunchers = {
-  sl_chrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 10',
-    version: '51.0'
-  },
-  sl_firefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 10',
-    version: '47.0'
-  },
-  sl_ie11: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 10',
-    version: '11.0',
-  },
-  sl_edge: {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '13.10586'
-  },
-  sl_safari: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.11',
-    version: '9.0',
-  },
-}
-
-const isTravis = process.env.TRAVIS === 'true'
-const isFirstJob = /\.1$/.test(process.env.TRAVIS_JOB_NUMBER)
-
-const browsers = [ isTravis ? 'ChromiumHeadless_without_security' : 'ChromeHeadless' ]
-if (isTravis && isFirstJob) {
-  browsers.push(...Object.keys(sauceLabsLaunchers))
-}
-
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['mocha', 'chai-spies', 'chai'],
 
-    browsers: browsers,
-    customLaunchers: Object.assign({
+    browsers: [ process.env.CI ? 'ChromiumHeadless_without_security' : 'ChromeHeadless' ],
+    customLaunchers: {
       ChromiumHeadless_without_security: {
         base: 'ChromiumHeadless',
         flags: [
@@ -57,11 +16,8 @@ module.exports = function(config) {
           '--disable-setuid-sandbox',
         ],
       },
-    }, sauceLabsLaunchers),
-    sauceLabs: {
-      public: 'public',
     },
-    concurrency: isTravis ? 5 : Infinity,
+    concurrency: 1,
     browserDisconnectTimeout: 10000,
     browserNoActivityTimeout: 10000,
     browserDisconnectTolerance: 16,
